1279bc27c6462a8d02cb638af3dd505823f845b7
Godswillomuka <omukagodswil@gmail.com>
2024-10-07 10:51:38 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+console.log("Script is running");
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = "Your Header Text";
+newHeader.textContent = "Your name is the champion"
+document.body.appendChild(newHeader);
+console.log(newHeader);
